26c8997d6575eb7843f7410027a9bad43c4a7714
Liubov Krivtsova <avostvirk@gmail.com>
2023-05-20 20:24:01 +0300

    feat: add login

--- index.js
+++ index.js
@@ -20,6 +20,40 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserSchema.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден', // Неверный логин или пароль
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign({
+      _id: user._id,
+    }, 'secret123', { expiresIn: '30d' });
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    });
+  }
+});
+
 app.post('/auth/register', registerValidator, async (req, res) => {
   try {
     const errors = validationResult(req);
