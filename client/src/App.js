70d3ee670056dbdd0eb1fb1dba5e40efa44db195
Liubov Krivtsova <avostvirk@gmail.com>
2023-05-23 22:55:12 +0300

    feat: improve App.js, index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Registration } from './pages/Registration';
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
+import { PeopleList } from './pages/PeopleList';
+import { Profile } from './pages/Profile';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
+      <Header />
       <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
+        <Route path='/' element={ <Home /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/register' element={ <Registration /> } />
+        <Route path='/account' element={ <Profile /> } />
+        <Route path='/people' element={ <PeopleList /> } />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
